f60786ffd88f09a8fcc97c6b468765f530aa2656
mwielocha <mikowaj@gmail.com>
2015-01-18 11:51:06 +0100

    initial commit

---
+++ Axaia/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Axaia
+//
+//  Created by Mikołaj Wielocha on 18/01/15.
+//  Copyright (c) 2015 Mikołaj Wielocha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
